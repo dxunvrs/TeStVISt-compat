056f676b9b0914218885bdc8c85615ad7d9b6a98
roma-dxunvrs <r.shulai@yandex.ru>
2026-04-29 15:02:52 +0300

    Fix problem with treated_wood and filling

--- src/main/java/com/dxunvrs/recipefixer/mixin/RecipeManagerMixin.java
+++ src/main/java/com/dxunvrs/recipefixer/mixin/RecipeManagerMixin.java
@@ -5,6 +5,7 @@ import com.google.gson.JsonObject;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.util.profiling.ProfilerFiller;
+import net.minecraft.world.item.crafting.RecipeManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Map;
 
-@Mixin(RecipeManagerMixin.class)
+@Mixin(RecipeManager.class)
 public class RecipeManagerMixin {
     @Inject(method = "apply", at = @At("HEAD"))
     private void onApply(Map<ResourceLocation, JsonElement> map,
